943b67f23164661908b014e4f6be06527e9c94f0
Selvakumar G <65029376+gselva28@users.noreply.github.com>
2020-08-03 20:12:08 +0530

    Add files via upload

---
+++ multi_KWpara_process.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 31 21:21:12 2020
+
+@author: SELVAKUMAR
+"""
+
+""" Keyword arguments which is used to create dictionary """
+
+def strr(**kwargs):
+    return kwargs
+
+print(strr(a=1,b=2,c=3))
